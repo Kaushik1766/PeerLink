ec3c5ff529bf588fc7d8a7f65a6e1842f302b69c
Prateek195jain <118386573+Prateek195jain@users.noreply.github.com>
2024-02-01 00:19:31 +0530

    Login Page added

--- client/PeerLink/src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-    return <>
-        <h1 className="text-center p-5 m-5" >
-            Hello world
-        </h1>
-    </>
-}
---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+import Login from "./components/Login"
+import "./index.css";
+export default function App() {
+    return <>
+        <Login />
+    </>
+}
