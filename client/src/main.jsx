23d80ef53113a5c0980b2c537f3fa18c7f12b75c
Prateek195jain <118386573+Prateek195jain@users.noreply.github.com>
2024-02-24 22:47:35 +0530

    navbar created

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.css';
-import PageRouter from './PageRouter.jsx'
-import App from './pages/Login.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+import PageRouter from "./PageRouter.jsx";
+import App from "./pages/Login.jsx";
+import { Provider } from "react-redux";
+import store from "./store.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <PageRouter />
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <PageRouter />
+  </Provider>
+);
