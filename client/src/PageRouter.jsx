843daf1a9e765dfb182e813886ea4d96578c2d98
Prateek195jain <118386573+Prateek195jain@users.noreply.github.com>
2024-03-10 14:37:10 +0530

    ddf

--- client/src/PageRouter.jsx
+++ client/src/PageRouter.jsx
@@ -2,13 +2,14 @@ import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Navbar from "./components/Navbar";
 import NotFound from "./pages/NotFound";
+import App from "./pages/App";
 
 export default function PageRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Navbar />} />
         <Route index element={<Login />} />
+        <Route path="/app" element={<App />} />
         {/* <Route path='/home' element={<App />} />
                 <Route path='/create' element={<Create />} /> */}
         <Route path="*" element={<NotFound />} />
